1465f808e95995c6a6e223f8059adfd14236b089
JohnCodePass <jmoralesc0259@gmail.com>
2021-05-26 14:46:09 -0700

    adding git and live to portfolio

--- portfolioApp/src/app/pages/mainpage/mainpage.component.ts
+++ portfolioApp/src/app/pages/mainpage/mainpage.component.ts
@@ -5,46 +5,59 @@ declare interface RouteInfo {
   pic: string;
   techUsed: string,
   desc: string;
+  git: string;
+  live: string;
 }
 export const ROUTES: RouteInfo[] = [
   {
-    title: "Hamster Hotel",
-    pic: "../../../assets/images/hamsterhomejpg.jpg",
-    techUsed: "HTML, CSS, BootStrap",
-    desc: "Individual project, where we were given a prototype as reference to create a website",
-  },
-  {
-    title: "Puppy Restaurant",
-    pic: "../../../assets/images/cateringHero.jpg",
-    techUsed: "HTML, CSS, BootStrap",
-    desc: "Group project of 2, where we were given a prototype as a reference to create a website",
-  },
-  {
-    title: "Business Redevelopment",
-    pic: "../../../assets/images/SHOP.jpg",
-    techUsed: "HTML, CSS, BootStrap",
-    desc: "Group project of 2, where we had to find a local business, create a prototype and recreate the website",
-  },
-
-  {
-    title: "Weather App",
-    pic: "../../../assets/images/night cloudy.jpg",
-    techUsed: "HTML, CSS, JavaScript, BootStrap, OpenWeather API",
-    desc: "Individual project, where the user enters a city and state to get the current, morning, evening, night time weather, and the 5 day week forecast",
+    title: "Health Force Partners",
+    pic: "../../../assets/images/HFPbg.png",
+    techUsed: "Velo by Wix, JavaScript, Adobe Xd",
+    desc: "COMING SOON",
+    git: "",
+    live: "",
   },
   {
     title: "QUp App",
     pic: "../../../assets/images/teemoLogin.jpg",
-    techUsed: "HTML, CSS, TypeScript, RIOT API, NGBootStrap, Angular 11, C#, ASP.NET",
-    desc: "Full Stack app that is currently in development with a total group of 3. ",
+    techUsed: "HTML, CSS, TypeScript, RIOT API, NGBootStrap, Angular 11, C#, ASP.NET, Adobe Xd",
+    desc: "Full Stack app that is currently in development.",
+    git: "",
+    live: "",
   },
   {
-    title: "Health Force Partners",
-    pic: "../../../assets/images/HFPbg.png",
-    techUsed: "COMING SOON",
-    desc: "COMING SOON",
+    title: "Business Redevelopment",
+    pic: "../../../assets/images/SHOP.jpg",
+    techUsed: "HTML, CSS, BootStrap, Adobe Xd",
+    desc: "Team project, where we had to find a local business, create a prototype and recreate the website",
+    git: "https://github.com/JohnCodePass/Project-4-Business-Redevelopment-Project",
+    live: "",
   },
-  
+  {
+    title: "Weather App",
+    pic: "../../../assets/images/night cloudy.jpg",
+    techUsed: "HTML, CSS, JavaScript, BootStrap, OpenWeather API, Adobe Xd",
+    desc: "Individual project, where the user enters a city and state to get the current, morning, evening, night time weather, and the 5 day week forecast. User can save cities to local storage.",
+    git: "",
+    live: "",
+  },
+  {
+    title: "Puppy Restaurant",
+    pic: "../../../assets/images/cateringHero.jpg",
+    techUsed: "HTML, CSS, BootStrap, Adobe Xd",
+    desc: "Team project, where we were given a prototype as a reference to create a website",
+    git: "",
+    live: "",
+  },
+  {
+    title: "Hamster Hotel",
+    pic: "../../../assets/images/hamsterhomejpg.jpg",
+    techUsed: "HTML, CSS, BootStrap, Adobe Xd",
+    desc: "Individual project, where we were given a prototype as reference to create a website",
+    git: "",
+    live: "",
+  },
+
 ];
 
 @Component({
